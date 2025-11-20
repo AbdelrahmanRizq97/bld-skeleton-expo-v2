1bdeefbf00f39748c18222321a44828d3434bdfd
rizq-dapi <62249591+AbdelrahmanRizq97@users.noreply.github.com>
2025-11-20 22:31:46 +0200

    fix metro configuration

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,30 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
+const { resolve } = require('metro-resolver');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver = config.resolver || {};
+config.resolver.alias = {
+  ...(config.resolver.alias || {}),
+  '@': path.resolve(__dirname),
+};
+
+const previousResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (typeof moduleName === 'string' && moduleName.startsWith('@/')) {
+    const absolutePath = path.resolve(__dirname, moduleName.slice(2));
+    try {
+      return resolve(context, absolutePath, platform);
+    } catch {
+      // If our absolute resolution fails, fall through to previous/default
+    }
+  }
+  if (typeof previousResolveRequest === 'function') {
+    return previousResolveRequest(context, moduleName, platform);
+  }
+  return resolve(context, moduleName, platform);
+};
 
 module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
